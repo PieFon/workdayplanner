4d33160ecf54100b0fa4155d45919731d090b67b
Pierre Fontecha <Pierre.Fontecha@gmail.com>
2020-12-17 22:20:21 -0800

    added index, save icon, script, style

---
+++ DayPlanner/script.js
@@ -0,0 +1,11 @@
+// var today = new Date();
+// vars date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+// var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+// var dateTime = date+' '+time;
+
+// // function showTime ()
+
+
+$("#saveBtn").on("click", function() {
+    alert("I've been clicked!");
+  });
